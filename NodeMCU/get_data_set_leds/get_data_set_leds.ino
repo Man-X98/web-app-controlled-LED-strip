246bbc2b1ca51f961a121903ce4c5c5fc1d88e65
Man-X98 <74245602+Man-X98@users.noreply.github.com>
2020-12-03 16:11:16 +0100

    Added files and remove personal WIFI settings

---
+++ NodeMCU/get_data_set_leds/get_data_set_leds.ino
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+//PinOut:
+//Pin 2: D4 --> Red in WebApp
+//Pin 0: D3 --> Green in WebApp
+//Pin 4: D2 --> Blue in WebApp
+
+const char* ssid = "ADD WIFI SSID HERE";
+const char* password = "ADD WIFI PASSWORD HERE";
+
+//Your Domain name with URL path or IP address with path
+String serverIP = "http://192.168.0.20"; //Replace IP with IP of your raspberry
+String serverName = serverIP+":80/get_values";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+void setup() {
+  //Set all PWM Pins dutycyles to 0 (--> turn leds off)
+  analogWrite(2, 0);
+  analogWrite(0, 0);
+  analogWrite(4, 0);
+        
+  Serial.begin(9600); 
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      String serverPath = serverName;
+      http.begin(serverPath.c_str());
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      if (httpResponseCode>0) {        
+        String payload = http.getString();
+        //Split string and set PWM LEDs accordingly:        
+        int Values[3];
+        int i=0;
+        int startNum = 0;
+        int counter=0;
+        while (i<payload.length()){
+          if (payload.charAt(i)==','){
+            Values[counter]=payload.substring(startNum, i).toInt();
+            startNum=i+1;
+            counter = counter+1;
+          }
+          else if (i==(payload.length()-1)){
+            Values[counter]=payload.substring(startNum, i+1).toInt();
+          }
+          i=i+1;
+        }
+        //Set LEDs accordingly to recieved values:
+        analogWrite(2, Values[0]);
+        analogWrite(0, Values[1]);
+        analogWrite(4, Values[2]);          
+      }
+      else {
+        Serial.print("Error code: ");
+        
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
---
+++ WebApp/LED_app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request
+from gpiozero import PWMLED
+
+# Comment GPIO Part out to test on PC#
+
+
+# set up LEDs:
+# red = PWMLED(17)
+# green = PWMLED(27)
+# blue = PWMLED(22)
+red = 0
+green = 0
+blue = 0
+led_values = [0, 0, 0]
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if len(request.form) > 0:
+        led_values[0] = float(request.form["redvalue"])
+        print(led_values[0])
+        led_values[1] = float(request.form["greenvalue"])
+        led_values[2] = float(request.form["bluevalue"])
+
+    return render_template("basicTemplate.html", red=led_values[0], green=led_values[1], blue=led_values[2])
+
+
+@app.route("/off")
+def off():
+    led_values[0] = 0
+    print(led_values[0])
+    led_values[1] = 0
+    led_values[2] = 0
+    return render_template("basicTemplate.html", red=led_values[0], green=led_values[1], blue=led_values[2])
+
+
+@app.route("/on")
+def on():
+    led_values[0] = 1023
+    print(led_values[0])
+    led_values[1] = 1023
+    led_values[2] = 1023
+    return render_template("basicTemplate.html", red=led_values[0], green=led_values[1], blue=led_values[2])
+
+
+###mobile layout:
+@app.route("/mobile", methods=["GET", "POST"])
+def main_mobile():
+    if len(request.form) > 0:
+        led_values[0] = float(request.form["redvalue"])
+        print(led_values[0])
+        led_values[1] = float(request.form["greenvalue"])
+        led_values[2] = float(request.form["bluevalue"])
+
+    return render_template("mobileTemplate.html", red=led_values[0], green=led_values[1], blue=led_values[2])
+
+
+@app.route("/mobile/off")
+def off_mobile():
+    led_values[0] = 0
+    print(led_values[0])
+    led_values[1] = 0
+    led_values[2] = 0
+    return render_template("mobileTemplate.html", red=led_values[0], green=led_values[1], blue=led_values[2])
+
+
+@app.route("/mobile/on")
+def on_mobile():
+    led_values[0] = 1023
+    print(led_values[0])
+    led_values[1] = 1023
+    led_values[2] = 1023
+    return render_template("mobileTemplate.html", red=led_values[0], green=led_values[1], blue=led_values[2])
+
+
+@app.route("/get_values")
+def get_values():
+    # red_int = int(led_values[0])
+    # print("Value send to Arduino" + str(led_values[0]))
+    str_return = str(int(led_values[0])) + "," + str(int(led_values[1])) + "," + str(int(led_values[2]))
+    return str_return
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
